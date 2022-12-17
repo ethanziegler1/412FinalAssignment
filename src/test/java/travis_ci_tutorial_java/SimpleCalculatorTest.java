e086af8049bd505a4077b10d13b49efc51bff0bf
EthanZiegler <ethan.ziegler1@gmail.com>
2022-12-17 01:37:59 -0500

    added divide method and test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -16,4 +16,19 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.sub(5, 3), 2);
 	}
+	@Test
+	public void testMult(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mult(2, 3), 6);
+	}
+//	@Test
+//	public void testDiv(){
+//		SimpleCalculator calc = new SimpleCalculator();
+//		assertEquals(calc.div(2, 2), 1);
+//	}
+//	@Test
+//	public void testDivBy0(){
+//		SimpleCalculator calc = new SimpleCalculator();
+//		assertEquals(calc.div(5, 0), 0);
+//	}
 }
